89ccdfdb2f35e42b51d9ec6d48970d66e96aedca
shahriarsohan <sohanmock@gmail.com>
2023-02-08 22:52:22 +0600

    base path updated

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/dev/bkash-api",
+
   reactStrictMode: true,
   images: {
     domains: ["redshop.s3.ap-south-1.amazonaws.com"],
